516b46a48edf030518b7b6044ae4c5dca26ba74d
sighmon <simon.loffler@gmail.com>
2018-10-14 17:02:21 +1100

    Initial Commit

---
+++ Person Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Person Counter
+//
+//  Created by Simon Loffler on 14/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
